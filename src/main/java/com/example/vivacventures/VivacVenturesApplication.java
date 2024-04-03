9453b6900cdfa4ded4e2a1c5548c3dcf8d1cd35f
Pablopume <pablo.serrano32@educa.madrid.org>
2024-04-03 20:07:32 +0200

    Initial commit

---
+++ src/main/java/com/example/vivacventures/VivacVenturesApplication.java
@@ -0,0 +1,13 @@
+package com.example.vivacventures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivacVenturesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VivacVenturesApplication.class, args);
+    }
+
+}
