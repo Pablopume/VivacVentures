2e3da29e4be49ce7c547114fc6d9748862945aff
david <david.roldanoteo@educa.madrid.org>
2024-05-23 20:23:11 +0200

    AddValoration

--- src/main/java/com/example/vivacventures/data/repository/ValorationRepository.java
+++ src/main/java/com/example/vivacventures/data/repository/ValorationRepository.java
@@ -1,6 +1,8 @@
 package com.example.vivacventures.data.repository;
 
 import com.example.vivacventures.data.modelo.ValorationEntity;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -15,6 +17,8 @@ public interface ValorationRepository extends ListCrudRepository<ValorationEntit
 
     ValorationEntity findById(int id);
 
+    @Modifying
+    @Transactional
     @Query(value = "INSERT INTO valoration (vivac_id, username, score, review, date) VALUES (:vivac_id, :username, :score, :review, :date)", nativeQuery = true)
     void insertValoration(@Param("vivac_id") int vivacId, @Param("username") int username, @Param("score") int score, @Param("review") String review, @Param("date") LocalDate date);
 }
